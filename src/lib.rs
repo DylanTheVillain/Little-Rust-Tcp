720ccde270f19155bd1451ccd3679c55bc46ecf3
Dylan Johnson <dylanatasmsa@gmail.com>
2014-11-16 14:02:15 -0600

    Initial Commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 #[test]
-fn it_works() {
-}
+pub mod tcpclient;
+pub mod tcpserver;
---
+++ src/tcpserver.rs
@@ -0,0 +1,56 @@
+use std::io::{TcpListener, TcpStream};
+use std::io::{Acceptor, Listener};
+use std::string::String;
+
+pub struct Server
+{
+    port: String,
+    ip: String
+}
+
+pub trait ServerFunction
+{
+    fn new(new_port: String, new_ip: String) -> Server;
+    fn start_server(&self);
+    fn format_ip(&self) -> String;
+    fn get_port(&self) -> String;
+    fn get_ip(&self) -> String;
+}
+
+impl ServerFunction for Server
+{
+    fn new(new_port: String, new_ip: String) -> Server
+    {
+        return Server{port: new_port, ip: new_ip};
+    }
+
+    fn start_server(&self)
+    {
+        let ip = self.format_ip();
+        let listener = TcpListener::bind(ip.as_slice());
+        let mut acceptor = listener.listen();
+    }
+
+    fn format_ip(&self) -> String
+    {
+        let mut complete_ip = String::new();
+        complete_ip.push_str(self.ip.as_slice());
+        complete_ip.push_str(":");
+        complete_ip.push_str(self.port.as_slice());
+        return complete_ip;
+    }
+
+    fn get_port(&self) -> String
+    {
+        let mut port = String::new();
+        port.push_str(self.port.as_slice());
+        return port;
+    }
+
+    fn get_ip(&self) -> String
+    {
+        let mut ip = String::new();
+        ip.push_str(self.ip.as_slice());
+        return ip;
+    }
+}
