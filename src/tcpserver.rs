407a311179d78867b9b383bd192dcd9fdb42b0d6
Dylan Johnson <dylanatasmsa@gmail.com>
2014-11-23 00:11:18 -0600

    Test

--- src/tcpserver.rs
+++ src/tcpserver.rs
@@ -31,13 +31,17 @@ impl ServerFunction for Server
         let mut acceptor = listener.listen().unwrap();
         for opt_stream in acceptor.incoming()
         {
-            let mut stream = opt_stream.unwrap();
-            stream.write(b"Hello World!!\r\n");
+            spawn(test());
             // spawn(proc(){
             //     let mut stream = opt_stream.unwrap();
             //     stream.write(b"Hello World\r\n").unwrap();
             // })
         }
+    }
+
+    fn test()
+    {
+        println!("Hell");
     }
 
     fn format_ip(&self) -> String
